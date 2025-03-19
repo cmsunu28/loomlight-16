4b97f0fd43a26d00b02afa5f042d303a5fbbd83e
Christine <cmsunu28@users.noreply.github.com>
2025-03-18 22:13:21 -0700

    with correct diagram and code

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ WS2812Serial leds(numled, displayMemory, drawingMemory, ledpin, WS2812_GRB);
 #define FLIP_DOWN2  0x227093
 #define NEUTRAL  0x888888
 
-const int forwardbuttonpin = 23;
-const int backbuttonpin = 33;
+const int forwardbuttonpin = 40;
+const int backbuttonpin = 41;
 int forwardButtonState = 0; // 0: not pressed. 1: pressed. 2: debounced. 3: just released
 int backButtonState = 0;
 
